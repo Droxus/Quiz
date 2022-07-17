e6bf8bb68290ac52ae5ce43537c695f5b31879de
Droxus <alexredov@gmail.com>
2022-07-17 18:18:45 +0300

    -folder Quiz

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+// export default {
+//   name: 'app',
+//   components: {
+//     HelloWorld
+//   }
+//   }
+// const HelloWorld = HelloWorld()
 </script>
 
 <template>
-
+ <div id="app">
+    <!-- <HelloWorld /> -->
+ </div>
   
 </template>
 
