8c737bf9c25bdf657f5251d4cfd160db9ab6619e
Droxus <alexredov@gmail.com>
2022-07-18 15:42:20 +0300

    +title

---
+++ assets/CreatePack.316171cf.7eed35e2.js
@@ -0,0 +1,1 @@
+import{V as r,a as t,Q as e,U as s,X as l,c as n,r as c,M as o}from"./index.e21f8d4f.js";const u={},i=r("h1",null,"Create Pack",-1),d=t("Back");function f(k,p){const a=s("router-link");return l(),n("div",null,[i,c(a,{to:"/"},{default:o(()=>[d]),_:1})])}var m=e(u,[["render",f]]);export{m as default};
