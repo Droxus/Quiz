aa8b042f8a3944963fe5de7516e93f3ca970e60a
Droxus <alexredov@gmail.com>
2022-07-18 16:42:30 +0300

    file exist fix

---
+++ Quiz/src/views/CreatePack.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="createPacke">
+    <!-- <h1>Create Pack</h1> -->
+    <router-link :to="'/'">Back</router-link>
+    <div class="tabelQuestionCreate">
+        aa
+    </div>
+</div>
+</template>
+
+<style scoped>
+.tabelQuestionCreate{
+    width: 90vw;
+    height: 90vh;
+    display: grid;
+    grid-template-columns: repeat(10 1fr);
+    grid-template-rows: repeat(8 1fr);
+}
+</style>
