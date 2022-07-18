d60dabd658d1077808e1b54d2d7f7db105bc2607
Droxus <alexredov@gmail.com>
2022-07-18 15:28:21 +0300

    +menu but built

---
+++ assets/JoinGame.89fd19c9.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as a,c as n,a as r,w as c,b as s,d as _}from"./index.87caa4c8.js";const l={},d=s("h1",null,"Join Game",-1),i=_("Back");function m(u,f){const e=t("router-link");return a(),n("div",null,[d,r(e,{to:"/"},{default:c(()=>[i]),_:1})])}var h=o(l,[["render",m]]);export{h as default};
