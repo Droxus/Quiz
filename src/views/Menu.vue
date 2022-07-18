3ddf4f1a60779f090ecc553979ae0b27741c7f3b
Droxus <alexredov@gmail.com>
2022-07-18 18:54:53 +0300

    +menu

---
+++ src/views/Menu.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Menu</h1>
+    <ul>
+        <li><router-link :to="'/joinGame'">Join Game</router-link></li>
+        <li><router-link :to="'/createGame'">Create Game</router-link></li>
+        <li><router-link :to="'/findPack'">Find Pack</router-link></li>
+        <li><router-link :to="'/createPack'">Create Pack</router-link></li>
+    </ul>
+</template>
+
+<style>
+
+</style>
