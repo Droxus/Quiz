9e8571d51107c6fefa663a219c4a05c933167d91
Droxus <alexredov@gmail.com>
2022-07-19 13:12:10 +0300

    deploy

---
+++ docs/assets/CreateGame.da32c6bc.js
@@ -0,0 +1,1 @@
+import{_ as t,r as o,o as a,c as r,a as c,w as n,b as s,d as _}from"./index.1c12ceb8.js";const l={},d=s("h1",null,"Create Game",-1),i=_("Back");function m(u,f){const e=o("router-link");return a(),r("div",null,[d,c(e,{to:"/"},{default:n(()=>[i]),_:1})])}const h=t(l,[["render",m]]);export{h as default};
